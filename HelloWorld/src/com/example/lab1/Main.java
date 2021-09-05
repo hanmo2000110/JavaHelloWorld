bc705f018e4cc391985fba366a3dbaac02729b0d
kimshin <hanmo2000110@gmail.com>
2021-09-05 16:25:01 +0900

    i added my student number

--- HelloWorld/src/com/example/lab1/Main.java
+++ HelloWorld/src/com/example/lab1/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("made by kimshin");
+		System.out.println("my student number is 22000120");
 		System.out.println("Hello World!!!\n");
 		System.out.print("이름 입력 : ");
 		String name = s.next();
